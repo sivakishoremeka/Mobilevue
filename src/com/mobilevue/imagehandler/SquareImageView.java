966a7890386e6fd286b6099cbd43704648dde826
sivakishore <sivakishore.meka@gmail.com>
2014-04-21 12:07:24 +0530

    channel icons with round corners
    channel icons with round corners

--- src/com/mobilevue/imagehandler/SquareImageView.java
+++ src/com/mobilevue/imagehandler/SquareImageView.java
@@ -1,11 +1,15 @@
 package com.mobilevue.imagehandler;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class SquareImageView extends ImageView
 {
+   public static float radius = 18.0f;  
     public SquareImageView(Context context)
     {
         super(context);
@@ -26,5 +30,16 @@ public class SquareImageView extends ImageView
     {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+    }
+    
+    
+    @Override
+    protected void onDraw(Canvas canvas) {
+    
+    	  Path clipPath = new Path();
+          RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+          clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+          canvas.clipPath(clipPath);
+    	super.onDraw(canvas);
     }
 }
